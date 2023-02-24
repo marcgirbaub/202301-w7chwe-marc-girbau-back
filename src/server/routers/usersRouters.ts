5dfc7820cafdf8d7b3c64bfa32122cb5e7498ee5
Marc Girbau <marc.girbau94@gmail.com>
2023-02-24 22:56:28 +0100

    Add loginUserSchema to login router

--- src/server/routers/usersRouters.ts
+++ src/server/routers/usersRouters.ts
@@ -1,8 +1,16 @@
+import { validate } from "express-validation";
 import { Router } from "express";
 import { loginUser } from "../controllers/usersControllers";
+import loginUserSchema from "../schemas/loginUserSchema";
+
+const loginRoute = "/login";
 
 const usersRouter = Router();
 
-usersRouter.post("/login", loginUser);
+usersRouter.post(
+  loginRoute,
+  validate(loginUserSchema, {}, { abortEarly: false }),
+  loginUser
+);
 
 export default usersRouter;
