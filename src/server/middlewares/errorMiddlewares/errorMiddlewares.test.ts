13961c5decc97f550a0086fd51ff6f6ab00b732a
Marc Girbau <marc.girbau94@gmail.com>
2023-02-24 19:03:25 +0100

    Test notFoundError middleware

---
+++ src/server/middlewares/errorMiddlewares/errorMiddlewares.test.ts
@@ -0,0 +1,21 @@
+import { type NextFunction, type Request, type Response } from "express";
+import { notFoundError } from "./errorMiddlewares";
+
+const res = {
+  status: jest.fn().mockReturnThis(),
+  json: jest.fn(),
+} as Partial<Response>;
+const req = {} as Request;
+const next = jest.fn() as NextFunction;
+
+afterEach(() => jest.clearAllMocks());
+
+describe("Given a notFoundError middleware", () => {
+  describe("When it receives a request", () => {
+    test("Then it should call its next method", () => {
+      notFoundError(req, res as Response, next);
+
+      expect(next).toHaveBeenCalled();
+    });
+  });
+});
